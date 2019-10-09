9894dc115b00ed1dbf40a04c8dbf45de1e41a0e6
Lai Dee Loon <dll29@cam.ac.uk>
2019-10-09 13:15:17 +0100

    Updated Quandl and Glassnode data pulling

--- scrape_coinfarm.py
+++ scrape_coinfarm.py
@@ -2,6 +2,9 @@ from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import pandas as pd
 import csv
+from datetime import datetime
+from pytz import timezone
+import os
 
 # NOTE: requests-html does not work in a Jupyter environment
 
@@ -14,27 +17,29 @@ resp.html.render(sleep=5)
 soup = BeautifulSoup(resp.html.html, 'html.parser')
 
 # Get short and long values
-values_dict = {'long_val': soup.find('span', {'id':'t_long'}).text,
-               'long_ratio': soup.find('span', {'id':'t_long2'}).text,
-               'short_ratio': soup.find('span', {'id':'t_short2'}).text,
-               'short_val': soup.find('span', {'id':'t_short'}).text}
+values_dict = {'Time (EST)': datetime.now(timezone('US/Eastern')),
+               'long_val': int(soup.find('span', {'id': 't_long'}).text[1:-4].replace(",", "")),
+               'long_ratio': float(soup.find('span', {'id': 't_long2'}).text[:-2])/100,
+               'short_val': int(soup.find('span', {'id': 't_short'}).text[1:-4].replace(",", "")),
+               'short_ratio': float(soup.find('span', {'id': 't_short2'}).text[:-2])/100}
 
-# with open('data/Coinfarm/long_short_values.csv', 'w', newline='') as file:
-#     writer = csv.DictWriter(file, fieldnames=['long_val', 'long_ratio', 'short_ratio', 'short_val'])
-#     writer.writeheader()
-#     writer.writerows(values_dict)
-
-with open('data/Coinfarm/long_short_values.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    for key, value in values_dict.items():
-        writer.writerow([key, value])
+filepath = 'data/Coinfarm/long_short_values.csv'
+if os.path.isfile(filepath):
+    with open(filepath, 'a', newline='') as csv_file:
+        dict_writer = csv.DictWriter(csv_file, fieldnames=values_dict.keys())
+        dict_writer.writerow(values_dict)
+else:
+    with open(filepath, 'w', newline='') as csv_file:
+        dict_writer = csv.DictWriter(csv_file, fieldnames=values_dict.keys())
+        dict_writer.writeheader()
+        dict_writer.writerow(values_dict)
 
 # Get table data
-td_tag = soup.body.center.find('table', {'style':"background:#253440;"},
-                               recursive=False).find('td',{"style":"background:url('/public/bg_2.png') no-repeat ;"})
-table_tag = td_tag.table.tbody.find_all('tr',recursive=False)[7]
-table_long = table_tag.find('div', {'id':'vm_long'}).table.tbody.find_all('tr')
-table_short = table_tag.find('div', {'id':'vm_short'}).table.tbody.find_all('tr')
+td_tag = soup.body.center.find('table', {'style': "background:#253440;"},
+                               recursive=False).find('td', {"style": "background:url('/public/bg_2.png') no-repeat ;"})
+table_tag = td_tag.table.tbody.find_all('tr', recursive=False)[7]
+table_long = table_tag.find('div', {'id': 'vm_long'}).table.tbody.find_all('tr')
+table_short = table_tag.find('div', {'id': 'vm_short'}).table.tbody.find_all('tr')
 
 # Scrape for header values in the table
 headers = []
@@ -50,4 +55,8 @@ for idx, table in enumerate([table_long, table_short]):
         long_dict = dict((headers[j], table[i].find_all('td')[j+1].text) for j in range(len(headers)))
         row_list.append(long_dict)
     table_df = pd.DataFrame(row_list, columns=headers)
-    table_df.to_csv('data/Coinfarm/' + filename[idx] + '.csv', index=False)
+    filepath = 'data/Coinfarm/' + filename[idx] + '.csv'
+    if os.path.isfile(filepath):
+        table_df.to_csv(filepath, index=False, mode='a', header=False)
+    else:
+        table_df.to_csv(filepath, index=False)
